1e0aa869eb9737a388551a3fde3cde7ff3d24071
morthasandeep777 <morthasandeep777@gmail.com>
2026-02-28 21:21:43 +0530

    week 2(a)

--- week2.R
+++ week2.R
@@ -1,92 +1,67 @@
-# Load ggplot2
+#Load the dataset
+AirPassengers
+?AirPassengers
+
+#Verify the Dataset
+class(AirPassengers)
+View(AirPassengers)
+
+ap_df
+
+ap_df<-data.frame(
+  year=time(AirPassengers),
+  passengers=as.numeric(AirPassengers)
+)
+ap_df
+
+#Convert the Dataset to dataframe
+ap_df_months<-data.frame(
+  year=floor(time(AirPassengers)),
+  month=cycle(time(AirPassengers)),
+  passengers=as.numeric(AirPassengers)
+)
+
+ap_df_months
+
+plot(AirPassengers,
+     type='l',
+     lwd=2,
+     main="Line Chart of AirPassengers",
+     xlab="Years",
+     ylab="Passengers",
+     col="red",
+)
+
+points(AirPassengers,
+       type='o',
+       pch=17,
+       col="blue"
+)
+
 library(ggplot2)
 
 
-# Count bar chart
-ggplot(iris, aes(x = Species)) +
-  geom_bar()
-# ggplot counts automatically
-
-
-# LAYER 7: STYLED BAR CHART (ggplot2)
-ggplot(iris, aes(x = Species)) +
-  geom_bar(fill = "steelblue") +
+ggplot(ap_df, aes(x=year,y = passengers)) +
+  geom_line(color = "blue", linewidth = 1) +
   labs(
-    title = "Distribution of Iris Species",
-    y = "Count"
+    title = "Line Chart of Air Passengers",
+    caption="Using Air Passengers Dataset",
+    subtitle="From 1949-1960",
+    x = "Year",
+    y = "Passengers"
   ) +
-  theme_minimal()
-
-
-# LAYER 8: BAR CHART WITH AGGREGATION (ggplot2)
-
-# Mean Sepal Length per Species
-ggplot(iris, aes(x = Species, y = Sepal.Length)) +
-  stat_summary(
-    fun = mean,
-    geom = "bar",
-    fill = "orange"
-  ) +
-  labs(
-    title = "Mean Sepal Length by Species",
-    y = "Mean Sepal Length"
-  ) +
-  theme_minimal()
-# stat_summary() performs aggregation
+  geom_point(color="red")+
+  geom_smooth(se=FALSE,color='black')+
+  theme_light()+
+    theme(
+      axis.title.x = element_text(color = "darkgreen", size = 12, face = "bold"),
+      axis.title.y = element_text(color = "purple", size = 12, face = "bold"),
+      axis.text.x  = element_text(color = "brown", size = 10),
+      axis.text.y  = element_text(color = "blue", size = 10),
+      plot.title=element_text(color="orange",size=14)
+    )
 
 
 
-# LAYER 9: GROUPED BAR CHART (ggplot2)
-ggplot(iris, aes(x = Species, fill = Species)) +
-  geom_bar(position = "dodge") +
-  theme_minimal()
-
-# Or with aggregated values:
-ggplot(iris, aes(x = Species, y = Sepal.Length, fill = Species)) +
-  stat_summary(
-    fun = mean,
-    geom = "bar",
-    position = "dodge"
-  ) +
-  theme_minimal()
-# Easy comparison across categories
 
 
-# LAYER 10: STACKED BAR CHART (ggplot2)
-# Count-based stacked bar chart
-ggplot(iris, aes(x = Species, fill = Species)) +
-  geom_bar(position = "stack") +
-  theme_minimal()
-
-
-# Stacked bar with another categorical variable (example)
-ggplot(iris, aes(x = Species, fill = cut(Sepal.Length,50))) +
-  geom_bar(position = "stack") +
-  labs(fill = "Sepal Length Group") +
-  theme_minimal()
-# Shows sub-category composition
-
-
-
-# Or with aggregated values:
-library(tidyr)
-
-iris_long <- pivot_longer(
-  iris,
-  cols = c(Sepal.Length, Petal.Length),
-  names_to = "Measurement",
-  values_to = "Value"
-)
-
-# Plot
-ggplot(iris_long, aes(x = Species, y = Value, fill = Measurement)) +
-  stat_summary(
-    fun = mean,
-    geom = "bar",
-    position = "stack"
-  ) +
-  labs(
-    title = "Stacked Bar Chart of Mean Measurements (Iris)",
-    y = "Mean Value"
-  ) +
-  theme_minimal()
